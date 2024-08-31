ea888e26b434b8eb3ab96c980ba41f715ea17c72
Glitch (glitch-hello-website) <none>
2024-08-31 19:09:32 +0000

    ⭐️🌺 Checkpoint ./style.css:1908428/644 ./script.js:1908428/1905 ./index.html:1908428/32

--- script.js
+++ script.js
@@ -6,29 +6,45 @@
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
 console.log("Hello 🌎");
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
+function setTheme(themeName) {
+	localStorage.setItem("theme", themeName);
+	document.documentElement.className = themeName + "-theme";
+
+	let current = document.querySelector(".theme-picker li.current");
+	if (!current) {
+		return;
+	}
+	current.classList.remove("current");
+
+	current = document.querySelector(
+		".theme-picker li." + themeName + "-theme"
+	);
+	if (!current) {
+		return;
+	}
+	current.classList.add("current");
 }
 
+// Set theme before page renders
+(function () {
+	setTheme(localStorage.getItem("theme") || "logo");
+})();
 
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
+// When document ready add event listeners
+document.addEventListener("DOMContentLoaded", function () {
+	const selectTheme = (e) => {
+		const btn = e.currentTarget;
+		if (!btn.dataset.theme) {
+			return;
+		}
+		setTheme(btn.dataset.theme);
+	};
 
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
+	Array.from(document.getElementsByClassName("theme-selector-btn")).forEach(
+		(el) => {
+			el.addEventListener("click", selectTheme);
+		}
+	);
+
+	setTheme(localStorage.getItem("theme") || "neon");
 });
